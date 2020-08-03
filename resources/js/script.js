2129e2b4aaafd9a5370e6785c0e2ebd94f051548
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-08-02 23:27:24 -0700

    7.6
    scrolling animations for
    - features
    - steps
    - cities
    - plans

--- resources/js/script.js
+++ resources/js/script.js
@@ -20,6 +20,7 @@ $(document).ready(function() {
         $('html,body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
 
+    //Navigation scroll
     // Select all links with hashes
     $('a[href*="#"]')
     // Remove links that don't actually link to anything
@@ -57,5 +58,29 @@ $(document).ready(function() {
         }
     });
 
+    //animations on scroll
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    })
 
 });
